ff5d3761c21d539d26ed4f66ad7117fccc395ded
shenhav <snhvm0@gmail.com>
2021-08-09 14:23:54 +0300

    create basic table

--- client/src/app/components/Page2/Page2.js
+++ client/src/app/components/Page2/Page2.js
@@ -1,7 +1,45 @@
+import { makeStyles } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import axios from '../axios';
 
+const useStyles = makeStyles(() => ({
+    app: {
+        display: 'grid',
+        gridTemplateColumns: '5% 90% 5%',
+        gridTemplateRows: '10% 80% 10%',
+        height: '100%',
+        width: '100%',
+    },
+    tabel: {
+        gridColumnStart: '2',
+        gridRowStart: '2',
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    header: {
+        display: 'flex',
+        flexDirection: 'row',
+        height: '40px',
+        justifyContent: 'space-around',
+        color: 'white'
+    },
+    headerItem: {
+        width: '100%',
+        backgroundColor: '#3f51b5',
+        paddingLeft: '10px',
+        "&:not(:last-child)": {
+            borderRight: '4px solid #bbb',
+        }
+    },
+    content: {
+        backgroundColor: '#dad6d6',
+        height: '100%',
+        overflowY: 'auto'
+    }
+
+}));
 export default ({ className }) => {
+    const classes = useStyles();
     const [response, setResponse] = useState('');
 
     useEffect(() => {
@@ -11,7 +49,42 @@ export default ({ className }) => {
         })()
     }, []);
 
-    return (<div className={className}>
-        <h1>{response}</h1>
+    return (<div className={`${className} ${classes.app}`}>
+        <div className={classes.tabel}>
+            <div className={classes.header}>
+                <div className={classes.headerItem}>title 1</div>
+                <div className={classes.headerItem}>title 2</div>
+                <div className={classes.headerItem}>title 3</div>
+            </div>
+            <div className={classes.content}>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+                <div>content</div>
+            </div>
+        </div>
     </div>);
 };
